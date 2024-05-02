bd778ff9a28e0b11f3735e81a16eeb930735d488
EmersonPaduan <emerson.paduan@gmail.com>
2024-05-02 19:36:02 -0300

    aula 10 - starter project

---
+++ codigos/aula10/spring02/src/test/java/br/paduan/spring02/Spring02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.paduan.spring02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
